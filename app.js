3bf2c73150c07f54ae8ab5978c395d23ad39aac3
jessada <ipnioko54@gmail.com>
2023-03-17 19:04:06 +0700

    Second commit v.02

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const main = document.getElementById("main");
 list_nav.addEventListener("click", function () {
   if(list_click.style.width== "250px"){
     list_click.style.width= "0px"
-    main.style.marginLeft ="0px"
+    //main.style.marginLeft ="0px"
   }else{
     list_click.style.width= "250px"
-    main.style.marginLeft ="250px"
+    //main.style.marginLeft ="250px"
   }
 });
 
